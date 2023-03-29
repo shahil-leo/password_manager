f7f6af3f657f1dd183f2fe80f965ea82b8e4e39c
shahil <shahilcode@gmail.com>
2023-03-29 14:57:50 +0530

    added firebase and envirnments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'llocks',
+    appId: '1:737896032488:web:8f8a167b716177f7c8415d',
+    storageBucket: 'llocks.appspot.com',
+    apiKey: 'AIzaSyBbSOSXN_ne9ONlxd0BZGQ-zG91PrBQ7kc',
+    authDomain: 'llocks.firebaseapp.com',
+    messagingSenderId: '737896032488',
+    measurementId: 'G-49C5WCWCX1',
+  },};
